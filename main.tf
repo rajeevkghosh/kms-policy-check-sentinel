29783da70b21d0f0c338bc20771f4f9e244472f6
Rajeev Ghosh <rajeevkghosh@gmail.com>
2021-10-11 20:04:39 +0530

    committing test cases

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "google_kms_crypto_key" "nav-key1" {
   name            = "cryopt-key-nav1"
   key_ring        = google_kms_key_ring.keyring11.id
   rotation_period = "7776000s"
-  skip_initial_version_creation = false
+  #skip_initial_version_creation = false
 
   lifecycle {
     prevent_destroy = true
